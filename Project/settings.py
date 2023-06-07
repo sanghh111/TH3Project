38aa0041b8f5885b96e2ff098e0a9e37c85dba2e
sanghh111 <sanghh111@gmail.com>
2023-06-07 17:43:49 +0700

    no mess

--- Project/settings.py
+++ Project/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/home/TH3Project/static/"
-
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    ""/home/TH3Project/TMDT/static/"",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
